567a07a86389cd8add01a018e952e3810c7dbaf1
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2020-01-21 20:33:52 +0200

    rewrote the whole thing to be compatible with the new database structure, and use more listeners that were added to MusicPlayer

--- lib/datacollection.dart
+++ lib/datacollection.dart
@@ -1,107 +1,104 @@
-import 'dart:core';
-import 'musicplayer.dart';
-import 'song.dart';
-import 'database.dart';
+import 'package:player/database.dart';
+import 'package:player/musicplayer.dart';
+import 'package:player/song.dart';
 
 class MusicMonitor {
   MusicPlayer _musicPlayer;
-  SongProvider _songProvider;
+  DbProvider _dbProvider;
 
-  Future _updatePlayback(double newSeconds, double songProgress) async {
-    Playback lastPlayback = await _songProvider.getLastPlayback();
-    Song song = await _songProvider.getSong(lastPlayback.songId);
-    double secondsListened;
-    if (song.secondsListened == null)
-      secondsListened = 0;
-    else
-      secondsListened = song.secondsListened;
-    double totalSeconds = newSeconds + secondsListened;
-    await _songProvider.updateSongColumns(song,
-      <String, dynamic>{'secondsListened': totalSeconds});
-
-    _songProvider.updatePlaybackColumns(await lastPlayback,
-      <String, dynamic>{
-        'endDate': DateTime.now().toString(),
-        'progressOnEnd': songProgress
-      });
+  MusicMonitor(this._dbProvider, this._musicPlayer) {
+    _musicPlayer.addOnPositionChangeListener(_onPositionChangeListener);
+    _musicPlayer.addOnPauseListener(_onPauseListener);
+    _musicPlayer.addOnPlayListener(_onPlayListener);
+    _musicPlayer.addOnResumeListener(_onResumeListener);
+    // _musicPlayer.addOnCompleteListener(_onCompleteListener);
+    _musicPlayer.addOnSeekListener(_onSeekListener);
+    // _musicPlayer.addOnSkipListener(_onSkipListener);
   }
 
-  Future _startNewPlayback(int songId) async {
-    String datetime = DateTime.now().toString();
-    Playback playback = Playback(
-      songId: songId,
-      startDate: datetime,
-      endDate: datetime,
-      progressOnStart: _musicPlayer.progress,
-      progressOnEnd: -1,
-    );
-    await _songProvider.insertPlayback(playback);
-  }
-
-  void _onPlaySongListener(Song oldSong, Song newSong) {
+  void _onPlayListener(Song newSong) {
+    print('datacollection: new song \'' + newSong.name + '\'');
     _startNewPlayback(newSong.id);
   }
 
-  void _onProgressListener(double oldProgress,
-                           double newProgress,
-                           bool   seekedPosition) {
-    if (seekedPosition)
-      return;
-    double secondsListened = newProgress - oldProgress;
+  void _onPositionChangeListener(Duration newPosition) {
+    _dbProvider.getLastPlayback().then((Playback lastPlayback) {
+      _dbProvider.updatePlaybackColumns(lastPlayback, <String, dynamic>{
+        'endTimestamp': DateTime.now().millisecondsSinceEpoch,
+      });
+    });
+  }
 
-    /* happens after musicPlayer.seekPosition() is called */
-    if (secondsListened <= 0)
-      return;
-
-    _updatePlayback(secondsListened, newProgress);
-
-    // print('seconds to be logged ' + (secondsListened).toString());
+  void _onPauseListener() {
+    _dbProvider.getLastPlayback().then((Playback lastPlayback) {
+      _dbProvider.insertPause(lastPlayback.id,
+                                DateTime.now().millisecondsSinceEpoch);
+      print('datacollection: inserted pause');
+    });
   }
 
   void _onResumeListener() {
-    _startNewPlayback(_musicPlayer.currentSong.id);
+    _dbProvider.getLastPauseId().then((int lastPauseId) {
+      _dbProvider.insertResume(lastPauseId,
+          DateTime.now().millisecondsSinceEpoch);
+      print('datacollection: inserted resume');
+    });
   }
 
-  MusicMonitor(this._songProvider, this._musicPlayer) {
-    _musicPlayer.addOnPlaySongListener(_onPlaySongListener);
-    _musicPlayer.addOnProgressListener(_onProgressListener);
-    _musicPlayer.addOnResumeListener(_onResumeListener);
+  void _onCompleteListener() {
+    print('datacollection: complete, probs nothing to do now lol');
   }
+
+  void _onSeekListener(Duration newPosition) {
+    _dbProvider.getLastPlayback().then((Playback lastPlayback) {
+      _dbProvider.insertSeek(lastPlayback.id,
+                             newPosition.inMilliseconds / 1000,
+                             DateTime.now().millisecondsSinceEpoch);
+      print('datacollection: inserted seek');
+    });
+  }
+
+  void _onSkipListener(Song newSong) {
+    print('datacollection: skip');
+  }
+
+  Future _startNewPlayback(int songId) async {
+    Playback newPlayback = Playback(
+      songId: songId,
+      startTimestamp: DateTime.now().millisecondsSinceEpoch,
+      endTimestamp: -1,
+    );
+    _dbProvider.insertPlayback(newPlayback);
+  }
+
 }
 
 class Playback {
   int id;
   int songId;
-  String startDate;
-  String endDate;
-  double progressOnEnd;
-  double progressOnStart;
+  int startTimestamp;
+  int endTimestamp;
 
-  Playback({this.id, this.songId, this.startDate, this.endDate,
-            this.progressOnEnd, this.progressOnStart});
+  Playback({this.id,
+            this.songId,
+            this.startTimestamp,
+            this.endTimestamp});
 
-  Map<String, dynamic> toMap({bool withId = true}) {
-    Map<String, dynamic> map = {
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
       'songId': songId,
-      'startDate': startDate,
-      'endDate': endDate,
-      'progressOnEnd': progressOnEnd,
-      'progressOnStart': progressOnStart
+      'startTimestamp': startTimestamp,
+      'endTimestamp': endTimestamp,
     };
-    if (withId) {
-      map['id'] = id;
-    }
-    return map;
   }
 
   static Playback fromMap(Map<String, dynamic> map) {
     return Playback(
       id: map['id'],
       songId: map['songId'],
-      startDate: map['startDate'],
-      endDate: map['endDate'],
-      progressOnEnd: map['progressOnEnd'],
-      progressOnStart: map['progressOnStart'],
+      startTimestamp: map['startTimestamp'],
+      endTimestamp: map['endTimestamp'],
     );
   }
 }
