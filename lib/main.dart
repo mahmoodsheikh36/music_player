12b7e0a9e003564c55c96dbbadf4347fd8c0fbfc
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2020-01-21 23:47:23 +0200

    disabled the go back button from working in the app because it causes the main function to be run again when the app is brought back from the background

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:player/musicplayer.dart';
+import 'database.dart';
+import 'datacollection.dart';
 import 'root_widget.dart';
 
-void main() => runApp(App());
+void main() {
+  runApp(App());
+}
 
 Color textColor = Color(0xffddaa44);
 
 class App extends StatelessWidget {
+  final MusicPlayer _musicPlayer = new MusicPlayer();
+  final DbProvider _dbProvider = new DbProvider();
+  MusicMonitor _musicMonitor;
+
+  App() {
+    _musicMonitor = MusicMonitor(_dbProvider, _musicPlayer);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -28,7 +41,12 @@ class App extends StatelessWidget {
           subtitle: TextStyle(fontSize: 13.0, fontStyle: FontStyle.normal, color: Colors.black),
         ),
       ),
-      home: Root(),
+      home: WillPopScope(
+        child: Root(_dbProvider, _musicPlayer),
+        onWillPop: () async {
+          return false;
+        },
+      ),
     );
   }
 }
